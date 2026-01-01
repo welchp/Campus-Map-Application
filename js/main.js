87fc633a4ff1f96d3653e050af7f5261d83b987f
welchp <welch.parker@gmail.com>
2026-01-01 10:12:47 -0800

    added several layers

--- js/main.js
+++ js/main.js
@@ -74,6 +74,8 @@ var bikeshare_lyr;
 var student_spaces_lyr;
 var accessible_doors_lyr;
 var accessible_paths_lyr;
+var accessible_info_lyr;
+var classrooms_lyr;
 
 var foods = [];
 var transportations = [];
@@ -887,7 +889,8 @@ require([
 		portalItem:{
 			id:"bbfc05dc36cd4c7eb84ef3fdf00760fb"
 		},
-			visible: false
+		visible: false,
+		symbol:defaultMarker
 	})
 	
 	accessible_paths_lyr = new FeatureLayer({
@@ -903,17 +906,24 @@ require([
 		},
 			visible: false
 	})
-    
+
+	classrooms_lyr = new FeatureLayer({
+		portalItem:{
+			id:"2b8bd80d6f684a0d956d0fe41908e88c",
+		},
+			visible: false
+	})
+
     //Layer Groups  
   	foods = [cafes_lyr, perks_lyr, dining_halls_lyr, food_trucks_lyr]
   	transportations = [shuttles_lyr, metro_bus_lyr, bus_route_lyr, parking_lyr, bikeshare_lyr, bike_parking_lyr, bike_repair_lyr]
-  	student_life = [colleges_lyr, libraries_lyr, support_lyr, student_spaces_lyr]
-  	facilities = [construction_impacts_lyr, emergency_phones_lyr, genderinclusive_lyr, lactation_lyr, recycling_lyr, pdf_extents_lyr]
+  	student_life = [classrooms_lyr, colleges_lyr, libraries_lyr, support_lyr, student_spaces_lyr]
+  	facilities = [construction_impacts_lyr, emergency_phones_lyr, genderinclusive_lyr, lactation_lyr, recycling_lyr, pdf_extents_lyr, accessible_doors_lyr, accessible_paths_lyr, accessible_info_lyr]
   	recreations = [rec_lyr, gardens_lyr, poi_lyr]
 	buildings = [buildings_lyr]
   	allLayers = [foods, transportations, student_life, facilities, recreations, buildings] 
   	
-	everyLayer = [buildings_lyr, parking_lyr, bus_route_lyr, zones_lyr, libraries_lyr, support_lyr, student_spaces_lyr, shuttles_lyr, metro_bus_lyr, cafes_lyr, perks_lyr, food_trucks_lyr, bike_repair_lyr, dining_halls_lyr, bikeshare_lyr, bike_parking_lyr, bike_repair_lyr, genderinclusive_lyr, emergency_phones_lyr, lactation_lyr, recycling_lyr, gardens_lyr, poi_lyr, rec_lyr, colleges_lyr, labels_lyr, support_lyr, construction_impacts_lyr, pdf_extents_lyr]
+	everyLayer = [buildings_lyr, parking_lyr, bus_route_lyr, zones_lyr, libraries_lyr, support_lyr, student_spaces_lyr, shuttles_lyr, metro_bus_lyr, cafes_lyr, perks_lyr, food_trucks_lyr, bike_repair_lyr, dining_halls_lyr, bikeshare_lyr, bike_parking_lyr, bike_repair_lyr, genderinclusive_lyr, emergency_phones_lyr, lactation_lyr, recycling_lyr, gardens_lyr, poi_lyr, rec_lyr, colleges_lyr, labels_lyr, support_lyr, construction_impacts_lyr, pdf_extents_lyr, classrooms_lyr, accessible_paths_lyr, accessible_doors_lyr, accessible_info_lyr]
 	
 	
 	
